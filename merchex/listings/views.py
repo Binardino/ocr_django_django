d73529d4876cc62cfab31931b0c1ce1dfeae8b9a
Robin <langlois.robin@gmail.com>
2023-02-20 12:54:28 +0100

    add about-us view

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def hello(request):
-    return HttpResponse('<h1>Hello Django Unchained ! Djangooo Djangoooo </h1>')
+    return HttpResponse('<h1>Hello Django Unchained ! Djangooo Djangoooo</h1>')
+
+def about(request):
+    return HttpResponse('<h1>About us !</h1><p>We are Django Unchained fans & sell merchandising</p>')
